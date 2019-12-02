c3c88c3b704fd4d4181d5a049c17c02dd69c5473
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-02 22:40:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name != nil
+    if self.find_by_name(name) != nil
       return self.find_by_name(name)
     else
       self.create_by_name(name)
